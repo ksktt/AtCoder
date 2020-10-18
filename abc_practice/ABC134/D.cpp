4897728e0f4d0c20d7e429aa01a0c8a5a092aacf
Kusaka <popy73hongeop@gmail.com>
2020-10-18 17:11:32 +0900

    ABC134 C-D

--- abc_practice/ABC134/D.cpp
+++ abc_practice/ABC134/D.cpp
@@ -2,6 +2,34 @@
 using namespace std;
 
 int main() {
-    int n
+    int n;
+    cin >> n;
+    vector<int> a(n + 1);
+    vector<int> b(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        b[i] = 0;
+    }
+    for (int i = n; i >= 1; i--) {
+        int sum = 0;
+        for (int j = i; j < n+1; j += i) {
+            sum += b[j];
+        }
+        sum %= 2;
+        if (sum != a[i]) {
+            b[i] = 1;
+        }
+    }
+    vector<int> ans;
+    for (int i = 1; i <= n; i++) {
+        if (b[i] == 1) {
+            ans.push_back(i);
+        }
+    }
+    cout << ans.size() << endl;
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
