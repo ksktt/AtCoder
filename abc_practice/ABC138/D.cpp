b2961f20ecfa7ba78e792085e3e5396b48de8f54
Kusaka <popy73hongeop@gmail.com>
2020-10-22 13:53:35 +0900

    practice queue graph

--- abc_practice/ABC138/D.cpp
+++ abc_practice/ABC138/D.cpp
@@ -4,23 +4,37 @@ using namespace std;
 int main() {
     int n, q;
     cin >> n >> q;
-    vector<int> e(r);
+    vector<vector<int>> e(n);
     for (int i = 0; i < n - 1; i++) {
         int a, b;
         cin >> a >> b;
         a--;
         b--;
-        r[a].push_back(b);
-        r[b].push_back(a);
+        e[a].push_back(b);
+        e[b].push_back(a);
     }
-    vector<int> c(n);
+    map<int, int> c;
     for (int i = 0; i < q; i++) {
         int p, x;
         cin >> p >> x;
         p--;
-        s[p] += x;
+        c[p] += x;
     }
-    queue<int> q;
-
+    vector<int> ans(n, -1);
+    queue<int> que;
+	que.push(0);
+	while (que.size() > 0) {
+		int i = que.front();
+		que.pop();
+		for (int j : e[i]) {
+			if (ans[j] != -1) continue;
+            ans[j] = ans[i] + c[j];
+			que.push(j);
+		}
+	}
+    for (int i = 0; i < n; i++) {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
