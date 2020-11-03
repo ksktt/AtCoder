dc4bbdf7d8417bae94efdbf62682cbf3ff3beafa
Kusaka <popy73hongeop@gmail.com>
2020-11-03 09:39:24 +0900

    ac

--- abc_practice/ABC181/E.cpp
+++ abc_practice/ABC181/E.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+const ll inf = 1000000009;
+
 
 int main() {
     int n, m;
@@ -13,6 +15,15 @@ int main() {
         cin >> w[i];
     }
     sort(h.begin(), h.end());
-    
+    vector<int> sum1((n + 1) / 2), sum2((n + 1) / 2);
+    for (int i = 0; i < n - 1; i += 2) sum1[i / 2 + 1] = sum1[i / 2] + h[i + 1] - h[i];
+    for (int i = n - 2; i > 0; i -= 2) sum2[i / 2] = sum2[i / 2 + 1] + h[i + 1] - h[i];
+    int ans = inf;
+    for (int i : w) {
+        int x = lower_bound(h.begin(), h.end(), i) - h.begin();
+        if (x & 1) x ^= 1;
+        if (ans > sum1[x / 2] + sum2[x / 2] + abs(h[x] - i)) ans = sum1[x / 2] + sum2[x / 2] + abs(h[x] - i);
+    }
+    cout << ans << endl;
     return 0;
 }
